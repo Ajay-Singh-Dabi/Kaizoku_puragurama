2556c8d20f4d30ec4b4ea335e084a6e7fd340347
Ajay-Singh-Dabi <93148465+Ajay-Singh-Dabi@users.noreply.github.com>
2022-02-13 00:51:24 +0530

    Getting feed from camera
    On the feed it will show the Id and name of the employee
    which we will use for the access check

--- employee_detection.py
+++ employee_detection.py
@@ -1,2 +1,33 @@
 import cv2
-import face_recognition
+from employee_faces import EmployeesFacesList
+
+# we are getting the live feed from the door cameras
+capture = cv2.VideoCapture(0)
+
+# first lets encode the employee faces
+emp = EmployeesFacesList()
+emp.load_encoding_images("photos/")
+
+# Now if we need to continuous feed
+while True:
+    ret, frame = capture.read()
+    # Detect Faces
+    face_locations, face_names = emp.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, x2), (x2, y2), (0, 0, 200), 2)
+        s = ''.join(x for x in name if x.isdigit())
+        print(int(s))
+
+
+    cv2.imshow("Frame", frame)
+
+    # This will stop the camera feed
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
